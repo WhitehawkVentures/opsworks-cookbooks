96a90fbeb7251dde3631fe4aa59664f897923695
Steven Ou <stevenou@gmail.com>
2014-08-07 16:42:38 -0700

    try reload command

--- nginx/recipes/dapperman-redirect.rb
+++ nginx/recipes/dapperman-redirect.rb
@@ -1,5 +1,6 @@
 execute "nxensite dapperman-redirect" do
   command "/usr/sbin/nxensite dapperman-redirect"
-  notifies :reload, "service[nginx]"
+  command "/usr/sbin/nginx -s reload"
+  #notifies :reload, "service[nginx]"
   not_if do File.symlink?("#{node[:nginx][:dir]}/sites-enabled/dapperman-redirect") end
 end
