39d61d477ba1c732507cc2b717426bebee01ec3d
Steven Ou <stevenou@gmail.com>
2014-08-07 17:27:47 -0700

    try to include nginx service resource

--- nginx/recipes/dapperman-redirect.rb
+++ nginx/recipes/dapperman-redirect.rb
@@ -1,6 +1,8 @@
+include_recipe "nginx::service"
+
 execute "nxensite dapperman-redirect" do
   command "/usr/sbin/nxensite dapperman-redirect"
-  command "/usr/sbin/nginx -s reload"
-  #notifies :reload, "service[nginx]"
+  #command "/usr/sbin/nginx -s reload"
+  notifies :reload, "service[nginx]"
   not_if do File.symlink?("#{node[:nginx][:dir]}/sites-enabled/dapperman-redirect") end
 end
