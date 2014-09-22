1d1aa1f760b4823b74727bcf837276a8d01e56b5
Jay Brandenburg <jay@touchofmodern.com>
2014-09-22 14:27:59 -0700

    delete ruby/attributes/customize.rb

--- ruby/attributes/customize.rb
+++
@@ -1,7 +0,0 @@
-case node["opsworks"]["ruby_version"]
-when "2.1"
-  default[:ruby][:major_version] = '2'
-  default[:ruby][:minor_version] = '1'
-  default[:ruby][:patch_version] = '2'
-  default[:ruby][:pkgrelease] = '1'
-end
