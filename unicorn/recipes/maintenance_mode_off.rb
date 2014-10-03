a2db55f49f16dddf1442b2386abbfe803990ab9a
Jay Brandenburg <jay@touchofmodern.com>
2014-10-03 11:27:43 -0700

    maintenance_mode_on/_off recipes: looking for docroot opsworks attribute

--- unicorn/recipes/maintenance_mode_off.rb
+++ unicorn/recipes/maintenance_mode_off.rb
@@ -1,4 +1,4 @@
-execute "rm #{@application[:absolute_document_root]}maintenance_mode" do
-  command "/bin/rm #{@application[:absolute_document_root]}maintenance_mode"
+execute "rm #{deploy[:absolute_document_root]}maintenance_mode" do
+  command "/bin/rm #{deploy[:absolute_document_root]}maintenance_mode"
 end
 
--- unicorn/recipes/maintenance_mode_on.rb
+++ unicorn/recipes/maintenance_mode_on.rb
@@ -1,3 +1,3 @@
-execute "touch #{@application[:absolute_document_root]}maintenance_mode" do
-  command "/usr/bin/touch #{@application[:absolute_document_root]}maintenance_mode"
+execute "touch #{deploy[:absolute_document_root]}maintenance_mode" do
+  command "/usr/bin/touch #{deploy[:absolute_document_root]}maintenance_mode"
 end
