01c43fea2b255975962a7680a61c3dbbfe0fe6f9
Jay Brandenburg <jay@touchofmodern.com>
2014-10-03 14:42:40 -0700

    maintenance mode: get rid of separate cookbook for this

--- maintenance_mode/recipes/off.rb
+++
@@ -1,4 +0,0 @@
-execute "rm #{@application[:absolute_document_root]}maintenance_mode" do
-  command "/bin/rm #{@application[:absolute_document_root]}maintenance_mode"
-end
-
--- maintenance_mode/recipes/on.rb
+++
@@ -1,3 +0,0 @@
-execute "touch #{@application[:absolute_document_root]}maintenance_mode" do
-  command "/usr/bin/touch #{@application[:absolute_document_root]}maintenance_mode"
-end
